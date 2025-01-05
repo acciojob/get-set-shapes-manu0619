f1a99b982ea56e0020c6f676d3d6c4d18477326c
manu0619 <125184498+manu0619@users.noreply.github.com>
2025-01-06 02:08:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+  #width;
+  #height;
 
-class Square extends Animal {}
+  constructor(width, height) {
+    this.#width = width; // Initialize private field
+    this.#height = height; // Initialize private field
+  }
+
+  // Getter for width
+  get width() {
+    return this.#width;
+  }
+
+  // Getter for height
+  get height() {
+    return this.#height;
+  }
+
+  // Method to calculate area
+  getArea() {
+    return this.#width * this.#height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    // Call the parent class constructor with width and height as the same value (side)
+    super(side, side);
+  }
+
+  // Method to calculate perimeter
+  getPerimeter() {
+    return 4 * this.width; // Or 4 * this.side (both are the same for a square)
+  }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
